2bc1662ab7dbab42b0b43f07a9b8b6c4d072a189
Adxxs <adssx.dev@gmail.com>
2021-10-30 21:20:45 +0200

    Improving output visually

--- src/result_plotter.rs
+++ src/result_plotter.rs
@@ -22,7 +22,7 @@ impl ResultPlotter {
 
 impl DataExporter for ResultPlotter {
     fn export(&mut self, sim_result : &SimulationResult) -> Result<(), Box<dyn Error>> {
-        let mut title: String = "N=".to_owned();
+        let mut title: String = "Possible outcomes = 95; Number of simulations=".to_owned();
         title.push_str(&format!("{:08}", sim_result.total_number_of_simulations()));
         
         let folder = Path::new(&(self.folder));
@@ -30,25 +30,44 @@ impl DataExporter for ResultPlotter {
         self.current_id += 1;
         
         let root = BitMapBackend::new(&current_path, (800, 600)).into_drawing_area();
-        
+        //root.titled(, )?;
+
         let number_of_results = sim_result.total_number_of_simulations() as f64;
 
         root.fill(&WHITE)?;
 
-        let areas = root.split_by_breakpoints([944], [80]);
+        let (upper, lower) = root.split_vertically(50);
 
-        let mut scatter_ctx = ChartBuilder::on(&areas[2])
-            .caption(title, ("sans-serif", 50).into_font())
-            .x_label_area_size(40)
-            .y_label_area_size(40)
+        ChartBuilder::on(&upper)
+            .caption("Monte Carlo simulation of opening loot boxes", ("arial", 50).into_font().color(&BLUE))
+            .build_cartesian_2d(0..1, 0..1)?
+            .configure_mesh()
+            .draw()?;
+
+        let mut scatter_ctx = ChartBuilder::on(&lower)
+            .caption(title, ("arial", 25).into_font())
+            .x_label_area_size(60)
+            .y_label_area_size(80)
             .build_cartesian_2d(0f64..1020f64, 0f64..0.01f64)?;
+            
+
+        let gray = RGBColor(100, 100, 100);
+
         scatter_ctx
             .configure_mesh()
+            .light_line_style(&WHITE)
+            .x_label_style(("arial", 20).into_font().color(&gray))
+            .y_label_style(("arial", 20).into_font().color(&gray))
+            .x_label_formatter(&|v| format!("{:.0}", v))
+            .y_label_formatter(&|v| format!("{:.3}", v))
+            .x_desc("Number of lootboxes to get all possible outcomes")
+            .y_desc("Probability")
+            .axis_desc_style(("arial", 20))
             .draw()?;
 
         scatter_ctx.draw_series(
             sim_result.result_table.iter().enumerate()
-                .map(|(x, y)| Circle::new((x as f64, (*y as f64)/number_of_results), 1.5, Into::<ShapeStyle>::into(&BLUE).filled())).collect::<Vec<_>>()
+                .map(|(x, y)| Circle::new((x as f64, (*y as f64)/number_of_results), 2.0, Into::<ShapeStyle>::into(&BLUE).filled())).collect::<Vec<_>>()
         )?;
 
         // To avoid the IO failure being ignored silently, we manually call the present function
